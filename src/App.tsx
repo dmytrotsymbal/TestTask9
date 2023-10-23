f4405eb299552d5124c4475026a11466c8d295c4
Dima <dima.tsymbal@outlook.com>
2023-10-23 17:30:33 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import TablePage from "./pages/TablePage";
-import NotFoundPage from "./pages/NotFoundPage";
+import { Suspense, lazy } from "react";
 import { Container } from "react-bootstrap";
+import CustomLoader from "./components/CustomLoader";
+
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const TablePage = lazy(() => import("./pages/TablePage"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
 
 type Props = {};
 const App = (props: Props) => {
   return (
     <>
       <Container>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/table" element={<TablePage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+        <Suspense fallback={<CustomLoader />}>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/table" element={<TablePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
       </Container>
     </>
   );
---
+++ src/components/CustomLoader.tsx
@@ -0,0 +1,7 @@
+import "../styles/CustomLoader.scss";
+
+type Props = {};
+const CustomLoader = (props: Props) => {
+  return <div className="custom-loader"></div>;
+};
+export default CustomLoader;
