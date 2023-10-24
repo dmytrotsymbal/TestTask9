a2d50d629547b6de1d9012fcabefdf1bf69cf5c0
Dima <dima.tsymbal@outlook.com>
2023-10-24 12:58:55 +0300

    Alert animation

--- src/components/AlertMessage.tsx
+++ src/components/AlertMessage.tsx
@@ -1,14 +1,29 @@
 import { Alert } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import "../styles/AlertMessage.scss";
 
 type Props = {
   error: string | null;
   setError: (error: string | null) => void;
 };
+
 const AlertMessage = ({ error, setError }: Props) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (error) {
+      setIsVisible(true);
+      const timer = setTimeout(() => {
+        setIsVisible(false);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [error]);
+
   return (
-    <div className="alertMessageBackground">
+    <div className={`alertMessageBackground ${isVisible ? "visible" : ""}`}>
       <Alert
-        className="alertMessage"
+        className={`alertMessage ${isVisible ? "visible" : ""}`}
         variant="danger"
         onClose={() => setError(null)}
         dismissible
@@ -18,4 +33,5 @@ const AlertMessage = ({ error, setError }: Props) => {
     </div>
   );
 };
+
 export default AlertMessage;
